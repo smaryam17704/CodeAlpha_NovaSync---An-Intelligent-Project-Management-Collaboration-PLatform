21141b8a26caceff03910a98f5f33a26ada29066
Maryam Zakir <maryamzakir2004@gmail.com>
2026-08-22 16:35:23 +0000

    Freebuff: URGENT BUG: User registration is broken

--- src/convex/auth.ts
+++ src/convex/auth.ts
@@ -2,9 +2,22 @@
 
 import { convexAuth } from "@convex-dev/auth/server";
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
+import { Password } from "@convex-dev/auth/providers/Password";
 import { emailOtp } from "./auth/emailOtp";
 
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [emailOtp, Anonymous],
+  providers: [
+    Password({
+      profile(params, ctx) {
+        const name = (params.name as string) || "";
+        return {
+          name,
+          email: params.email as string,
+        };
+      },
+    }),
+    emailOtp,
+    Anonymous,
+  ],
 });
